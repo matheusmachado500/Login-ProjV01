837b27db5e215db786173db946a2f431b0195997
matheusmachado500 <matheushenriquemachado2005@gmail.com>
2023-10-19 13:40:52 -0300

    PhpMailer

--- Processas/processaEmail.php
+++ Processas/processaEmail.php
@@ -1,27 +1,28 @@
 <?php
+require '../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-function sendEmail() {
+function sendEmail()
+{
     $mail = new PHPMailer(true);
-
     try {
-        $mail->isSMTP(); 
-        $mail->Host = 'smtp.example.com'; 
-        $mail->SMTPAuth = true; 
-        $mail->Username = 'user@example.com'; 
-        $mail->Password = 'secret'; 
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; 
-        $mail->Port = 465; 
+        $mail->isSMTP();
+        $mail->Host = 'localhost';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'gestiprodemail@gmail.com';
+        $mail->Password = 'imqh mfgg unjo bmbx';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 587;
 
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User'); 
-        $mail->addAddress('ellen@example.com'); 
-        $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
-        $mail->isHTML(true); 
+        $mail->setFrom('gestiprodemail@gmail.com', 'Mailer');
+        $mail->addAddress('josemanuelguedes@gmail.com', 'José Guedes');
+        # $mail->addAddress('ellen@example.com'); 
+        # $mail->addReplyTo('info@example.com', 'Information');
+        # $mail->addCC('cc@example.com');
+        # $mail->addBCC('bcc@example.com');
+        $mail->isHTML(true);
         $mail->Subject = 'Here is the subject';
         $mail->Body = 'This is the HTML message body <b>in bold!</b>';
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
