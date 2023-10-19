4d6b0e59a64c75c7fbb559614cce589c61d4f9f4
matheusmachado500 <matheushenriquemachado2005@gmail.com>
2023-10-19 12:37:59 -0300

    aaa

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ session_start();
 	<link rel="stylesheet" href="css/style.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-
 	</head>
 
 	<body>
